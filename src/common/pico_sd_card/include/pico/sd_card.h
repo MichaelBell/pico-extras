747b40ec8b35d028c9d4fb216653b5bf34944ace
Mike Bell <mdb036@gmail.com>
2021-02-28 17:03:03 +0000

    Add option to byte swap on read
    # Conflicts:
    #	src/common/pico_sd_card/include/pico/sd_card.h

--- src/common/pico_sd_card/include/pico/sd_card.h
+++ src/common/pico_sd_card/include/pico/sd_card.h
@@ -20,11 +20,11 @@ extern "C" {
 #define SD_ERR_BAD_PARAM (-4)
 
 #ifndef PICO_SD_CLK_PIN
-#define PICO_SD_CLK_PIN 23
+#define PICO_SD_CLK_PIN 5
 #endif
 
 #ifndef PICO_SD_CMD_PIN
-#define PICO_SD_CMD_PIN 24
+#define PICO_SD_CMD_PIN 18
 #endif
 
 #ifndef PICO_SD_DAT0_PIN
@@ -40,6 +40,7 @@ int sd_init_1pin();
 int sd_readblocks_sync(uint32_t *buf, uint32_t block, uint block_count);
 int sd_readblocks_async(uint32_t *buf, uint32_t block, uint block_count);
 int sd_readblocks_scatter_async(uint32_t *control_words, uint32_t block, uint block_count);
+void sd_set_byteswap_on_read(bool swap);
 bool sd_scatter_read_complete(int *status, int *blocks_complete);
 int sd_writeblocks_async(const uint32_t *data, uint32_t sector_num, uint sector_count);
 bool sd_write_complete(int *status);
